641ca847dbd3de5d8032990e69b8ee331fb47068
niuyuemin <yueminn@yahoo.com>
2019-11-03 13:43:07 -0800

    react auto complete demo v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,120 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { fruits } from "./constants";
+
 
 const App: React.FC = () => {
+  const [inputWord, setInputWord] = useState("");
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+  const [activeSuggestionIndex, setActiveSuggestion] = useState<number>(-1);
+  const [showSuggestions, setShowSuggestions] = useState<boolean>(false);
+
+  const selectSuggestion = (index: number) => {
+    setInputWord(suggestions[index]);
+    setShowSuggestions(false);
+    setActiveSuggestion(-1);
+  };
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let newInput = event.currentTarget.value;
+    setInputWord(newInput);
+
+    setActiveSuggestion(-1);
+    if (newInput) {
+      setSuggestions(
+        fruits.filter(fruit =>
+          fruit.toLowerCase().includes(newInput.toLowerCase())
+        )
+      );
+      setShowSuggestions(true);
+    } else {
+      setSuggestions([]);
+      setShowSuggestions(false);
+    }
+  };
+
+  const onKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    //If user presses the down key
+    if (event.keyCode === 40) {
+      //Already reach the end of auto-complete suggestion
+      if (activeSuggestionIndex === suggestions.length - 1) {
+        return;
+      }
+      setActiveSuggestion(prevState => prevState + 1);
+    }
+    //if user presses the up key
+    else if (event.keyCode === 38) {
+      if (activeSuggestionIndex === -1) {
+        return;
+      }
+      setActiveSuggestion(prevState => prevState - 1);
+    }
+    //if user presses the enter key
+    else if (
+      event.keyCode === 13 
+    ) {
+      if(activeSuggestionIndex >= 0) selectSuggestion(activeSuggestionIndex);
+      else alert(`Successfully Submit ${inputWord}`);
+    }
+  };
+
+  const onMouseOverSuggestion = (index: number) => {
+    return (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+      setActiveSuggestion(index);
+    };
+  };
+
+  const onClick = (index: number) => {
+    return (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+      selectSuggestion(index);
+    };
+  };
+
+  const onSubmitButtonClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault();
+    alert(`Successfully Submit ${inputWord}`);  
+  };
+
+  const suggestionList = (
+    <ul className="suggestions">
+      {suggestions.map((suggestion, index) => {
+        let className;
+        // Flag the active suggestion with a class
+        if (index === activeSuggestionIndex) {
+          className = "suggestion-active";
+        }
+
+        let matchIndex = suggestion.toLowerCase().indexOf(inputWord.toLowerCase());
+        return (
+          <li
+            className={className}
+            key={suggestion}
+            onMouseOver={onMouseOverSuggestion(index)}
+            onClick={onClick(index)}
+          >
+            {suggestion.slice(0, matchIndex)}
+            <strong>{suggestion.slice(matchIndex, matchIndex+inputWord.length)}</strong>
+            {suggestion.slice(matchIndex+inputWord.length)}
+          </li>
+        );
+      })}
+    </ul>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className="base">
+     <div className="form-filed">
+        <input
+          type="text"
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          value={inputWord}
+        />
+        {showSuggestions && suggestionList}
+      </div>
+
+      <button className='submit-button' onClick={onSubmitButtonClick}>Submit</button>
     </div>
   );
-}
+};
 
 export default App;
